ec5ea998ac0f35b2371dd0583c24c926b4dcb4dd
MaiconHenrique93 <maycon.ofera@hotmail.com>
2024-03-18 21:13:07 -0300

    Revert "sm8250-common: Restart qcrild on modem subsystem restart"
    This reverts commit 9ca8bea867ae2b94922f3feee94fc4ff03dcf3a0.
    
    Change-Id: I567491079b5f9f214c3839ced93ba6ef6ff4a00d

--- common.mk
+++ common.mk
@@ -466,10 +466,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Subsystem state notifier
-PRODUCT_PACKAGES += \
-    subsystem_state_notifier
-
 # Telephony
 PRODUCT_PACKAGES += \
     MotoNrEnabler \
